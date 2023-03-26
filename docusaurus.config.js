f4467dbe894addc5c7a65691abebae489e50df23
Ashel Joseph Vasquez <vasquezpalominoashel@gmail.com>
2023-03-26 12:09:57 -0500

    modificando la url de produccion en la configuracion

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,48 +6,47 @@ const darkCodeTheme = require('prism-react-renderer/themes/nightOwl');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Backend NodeJS y MongoDB',
-  tagline: 'Programación Backend',
-  favicon: 'img/favicon1.ico',
+  title: "Backend NodeJS y MongoDB",
+  tagline: "Aprediendo a crear un backend con NodeJS, Express y MongoDB",
+  favicon: "img/favicon1.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://ashel1806.github.io/backend-nodejs-mongodb/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/backend-nodejs-mongodb/',
+  baseUrl: "/backend-nodejs-mongodb/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ashel1806', // Usually your GitHub org/user name.
-  projectName: 'backend-nodejs-mongodb', // Usually your repo name.
+  organizationName: "ashel1806", // Usually your GitHub org/user name.
+  projectName: "backend-nodejs-mongodb", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es'],
+    defaultLocale: "es",
+    locales: ["es"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ashel1806/backend-nodejs-mongodb.git',
+          editUrl: "https://github.com/ashel1806/backend-nodejs-mongodb.git",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,16 +56,16 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Backend NodeJS y MongoDB',
+        title: "Backend NodeJS y MongoDB",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/nodejs-icon.svg',
+          alt: "My Site Logo",
+          src: "img/nodejs-icon.svg",
         },
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Backend NodeJS y MongoDB, Inc. Built with Docusaurus.`,
       },
       prism: {
