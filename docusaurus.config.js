a736a641ccae081b87e5da04810526380ef8e3a7
Ashel Joseph Vasquez <vasquezpalominoashel@gmail.com>
2023-03-26 12:26:09 -0500

    modificando la configuracion

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon1.ico",
 
   // Set the production url of your site here
-  url: "https://ashel1806.github.io/backend-nodejs-mongodb/",
+  url: "https://ashel1806.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/backend-nodejs-mongodb/",
